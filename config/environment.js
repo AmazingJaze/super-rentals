b2327d27a4e6526270374a571021028b9e169582
Jesse Sharp <AmazingJaze@gmail.com>
2016-07-29 13:36:23 -0700

    adjusting paths again

--- config/environment.js
+++ config/environment.js
@@ -38,7 +38,7 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'gh-pages') {
     ENV.rootURL = "https://amazingjaze.github.io/super-rentals/";
   }
 
